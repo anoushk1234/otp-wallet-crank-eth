25c048ea5dd959909565a96a0f0003d17498a2fe
anoushk1234 <anoushka1828@gmail.com>
2022-12-04 00:11:13 +0530

    fuck it works

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 const cron = require('node-cron');
+// import { ethers } from 'ethers';
 const ethers = require('ethers');
 const dotenv = require('dotenv');
 const abi = require('./abi.json');
@@ -6,18 +7,24 @@ const base32 = require('base32.js');
 const cryptojs = require('crypto-js');
 const { HmacSHA1 } = cryptojs;
 dotenv.config();
-const provider = new ethers.providers.JsonRpcProvider(process.env.RPC, 137);
+const provider = new ethers.providers.JsonRpcProvider(
+  'https://polygon-mumbai.g.alchemy.com/v2/-XiAniAE0vW5WjcliOiC1D_0muNwiYSr',
+  80001
+);
 const signer = new ethers.Wallet(process.env.PV!, provider);
 const SecurusContract = new ethers.Contract(
-  '0x5dDBd4D534D29e1642E2f2a786d8e08771f5f9e2',
+  '0x7b95e2765C3f1fEA1ae950e218bFfD0e61710ef4',
   abi,
   signer
 );
+console.log('Contract Address:', SecurusContract.address);
 cron.schedule(`*/30 * * * * *`, async () => {
+  console.log('running a task every 30 seconds');
   const code = HmacSHA1(
     String(genCode(process.env.USER_SEED!)),
     process.env.SEED!
   ).toString();
+  console.log('code', code);
   const tx = await SecurusContract.changeOTP(code);
   console.log(tx);
 });
